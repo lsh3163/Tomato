4bbd167f8e7b0455276cb6caebfa275e1ec33629
Koozzi <kchy666@naver.com>
2020-07-27 04:32:18 +0900

    remote-monster demo

--- client/src/Routes/Room.js
+++ client/src/Routes/Room.js
@@ -1,89 +1,28 @@
 import React, { Component } from 'react'
 
-const __html = require('./Conference.html');
-const template = { __html: __html };
-
 export default class Room extends Component {
     render() {
-        return ( <div dangerouslySetInnerHTML={template} /> );
+        return (
+            <>
+            <div>
+                <video
+                    id="myVideo"
+                    class="remote-video center w-300 h-300"
+                    autoPlay
+                    muted
+                    controls
+                    playsInline
+                >
+                </video>
+                <a id="enterBtn" href="#">
+                    ENTER
+                </a>
+                <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/@remotemonster/sdk"></script>
+                <script src="./Conference.js"></script>
+                Test
+            </div>
+            </>
+        )
     }
 }
-
-
-// import React, { Component } from 'react'
-
-// export default class extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <ul></ul>
-//                 <div>
-//                     <div>
-//                         <nav>
-//                             <buttonn>
-//                                 <i></i>
-//                             </buttonn>
-//                             <h3>Tomato Study</h3>
-//                         </nav>
-//                         <div>
-//                             <h4>How to Use</h4>
-//                             <li>
-//                                 Enter 클릭시 'remon' 이라는 conference Room 에 접속. Leave 클릭시 방을 떠납니다.
-//                             </li>
-//                             <br />
-//                             <div>
-//                                 <div>
-//                                     <div>
-//                                         <div>
-//                                             <div>
-//                                                 <h6>Other Video</h6>
-//                                             </div>
-//                                             <div>
-//                                                 <main>
-//                                                     <div></div>
-//                                                 </main>
-//                                             </div>
-//                                         </div>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-//                                             <div>
-//                                                 <h6>Local Video</h6>
-//                                             </div>
-//                                             <main>
-//                                                 <video
-//                                                     className="myVideo"
-//                                                     autoPlay
-//                                                     muted
-//                                                     controls
-//                                                     playsInline
-//                                                 ></video>
-//                                                 <h6>Waiting</h6>
-//                                                 <div>
-//                                                     <div>
-//                                                         <span>
-//                                                             <a
-//                                                                 href="#"
-//                                                             >
-//                                                             Enter
-//                                                             </a>
-//                                                         </span>
-//                                                     </div>
-//                                                 </div>
-//                                             </main>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
-//                         <script src="https://cdn.jsdelivr.net/npm/@remotemonster/sdk"></script>
-//                         <script>
-//                             const enterBtn = document
-//                         </script>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
