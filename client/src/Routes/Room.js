6ede46178871179785092c7116a841971a5496b8
Koozzi <kchy666@naver.com>
2020-07-30 15:21:23 +0900

    Video in React

--- client/src/Routes/Room.js
+++ client/src/Routes/Room.js
@@ -1,45 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component, useLayoutEffect } from 'react';
 import ScriptTag from 'react-script-tag';
+import Remon from "@remotemonster/sdk";
+import initConference from './Conference';
 
-export default class Room extends Component {
-    render() {
-        return (
-            <div id="content">
-                <div>
-                    <h6>
-                    Other Videos
-                    </h6>
-                </div>
-                <main>
-                    <div id="otherVideos"></div>
-                </main>
-                <h6>
-                    Local Video
-                </h6>
-                <video
-                    id="myVideo"
-                    class="remote-video center w-300 h-300"
-                    autoplay
-                    muted
-                    controls
-                    playsinline
-                ></video>
-                <h6
-                    id="waitingTv"
-                    class="m-0 font-weight-bold text-primary"
-                >waiting</h6>
-                <div>
-                    <div>
-                        <span>
-                        <a id="enterBtn" href="#">ENTER</a>
-                        </span>
-                    </div>
-                </div>
+const Room = () => {
 
-                <ScriptTag type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></ScriptTag>
-                <ScriptTag type="text/javascript" src="https://cdn.jsdelivr.net/npm/@remotemonster/sdk"></ScriptTag>
-                <ScriptTag type="text/javascript" src="./Conference.js"></ScriptTag>
+    useLayoutEffect(() => {
+      initConference();
+    }, []);
+
+    return (
+        <div id="content">
+        <div>
+            <h6>
+            Other Videos
+            </h6>
+        </div>
+        <main>
+            <div id="otherVideos"></div>
+        </main>
+        <h6>
+            Local Video
+        </h6>
+        <video
+            id="myVideo"
+            class="remote-video center w-300 h-300"
+            autoplay
+            muted
+            controls
+            playsinline
+        ></video>
+        <h6
+            id="waitingTv"
+            class="m-0 font-weight-bold text-primary"
+        >waiting</h6>
+        <div>
+            <div>
+                <span>
+                <a id="enterBtn" href="#">ENTER</a>
+                </span>
             </div>
-        )
-    }
-}
+        </div>
+    </div>
+    )
+};
+
+export default Room;
