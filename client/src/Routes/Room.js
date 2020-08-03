e6ffa20fe01f06488b5bb219a0b581666a0b71b7
Koozzi <kchy666@naver.com>
2020-08-03 16:39:09 +0900

    test

--- client/src/Routes/Room.js
+++ client/src/Routes/Room.js
@@ -1,5 +1,4 @@
 import React, { Component, useLayoutEffect } from 'react';
-import Remon from "@remotemonster/sdk";
 import initConference from './Conference';
 
 const Room = () => {
@@ -10,37 +9,41 @@ const Room = () => {
 
     return (
         <div id="content">
-        <div>
-            <h6>
-            Other Videos
-            </h6>
-        </div>
-        <main>
-            <div id="otherVideos"></div>
-        </main>
-        <h6>
-            Local Video
-        </h6>
-        <video
-            id="myVideo"
-            className="remote-video center w-300 h-300"
-            autoPlay
-            muted
-            controls
-            playsInline
-        ></video>
-        <h6
-            id="waitingTv"
-            className="m-0 font-weight-bold text-primary"
-        >waiting</h6>
-        <div>
+            <h1 id="timer"></h1>
+            <h6 id="console">Console</h6>
+            <h6 id="facecnt">Facent</h6>
             <div>
-                <span>
-                <a id="enterBtn" href="#">ENTER</a>
-                </span>
+                <h6>
+                Other Videos
+                </h6>
             </div>
+            <main>
+                <div id="otherVideos"></div>
+            </main>
+            <h6>
+                Local Video
+            </h6>
+            <video
+                id="myVideo"
+                className="remote-video center w-300 h-300"
+                autoPlay
+                muted
+                controls
+                playsInline
+            ></video>
+            <h6
+                id="waitingTv"
+                className="m-0 font-weight-bold text-primary"
+            >waiting</h6>
+            <div>
+                <div>
+                    <span>
+                    <a id="enterBtn" href="#">ENTER</a>
+                    </span>
+                </div>
+            </div>
+            <canvas id="myChart" width="400" height="400">Canvas</canvas>
         </div>
-    </div>
     )
 };
 
